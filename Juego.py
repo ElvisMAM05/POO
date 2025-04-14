95e44dce7ea865cdc35f7d8c449f9a473ba966ef
ElvisMAM05 <eaguilar119500862@gmail.com>
2025-04-14 15:57:13 -0600

    first commit

---
+++ Juego.py
@@ -0,0 +1,30 @@
+class Juego:
+    def __init__(self, nombre_jugador):
+      
+        self.nombre_jugador = nombre_jugador
+        self.jugando = False
+
+    def iniciar_juego(self):
+        self.jugando = True
+        print(f"\n¡Bienvenido al juego, {self.nombre_jugador}!")
+        print("El juego ha comenzado..")
+        print("Aqui va el juego o los metodos de instancia donde vas a crear la logica del juego.")
+        self.solicitar_palabra()
+
+        print("¡Gracias por jugar!\n")
+
+    def salir(self):
+        print(f"\nHasta luego, {self.nombre_jugador}. ¡Vuelve pronto!")
+        
+        
+    def codificar():
+        pass
+    
+    def solicitar_palabra(self):
+        
+
+
+  
+    
+
+
