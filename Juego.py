bceb4f510fbec67c8300c4be0d4cd1d8bc7b4eee
ElvisMAM05 <eaguilar119500862@gmail.com>
2025-04-15 15:57:32 -0600

    Other commit

--- Juego.py
+++ Juego.py
@@ -3,21 +3,21 @@ class Juego:
       
         self.nombre_jugador = nombre_jugador
         self.jugando = False
-
+        self.contador = 0
     def iniciar_juego(self):
         self.jugando = True
         print(f"\n¡Bienvenido al juego, {self.nombre_jugador}!")
         print("El juego ha comenzado..")
         
-        key= input()
         mensaje=input("Introducir mensaje: ").upper()
-        
+        P_Cod=0
         print("¡Gracias por jugar!\n")
         
         Codificado=self.codificar(mensaje)
         descodificado=self.descodificar(mensaje)
-        print(f"El mensaje codificado es {Codificado} ")
-        print(f"El mensaje descodificado es {descodificado} ")
+        print(f"El mensaje codificado es {Codificado} "
+              "Y las palabras codificadas son",self.contador)
+        print(f"El mensaje descodificado es {descodificado}")
 
             
     def salir(self):
@@ -25,17 +25,20 @@ class Juego:
 
             
     def codificar(self,mensaje):
+        self.contador +=1
         Codificado=""
         Abecedario= "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
         for letra in mensaje:
             num=Abecedario.index(letra)
             Espacio_L=num+1
             Codificado+=Abecedario[Espacio_L]
+        print(f"contador {self.contador}")
         return Codificado
     
     
     def descodificar(self,mensaje):
         descodificado=""
+        self.contador +=1
         Abecedarioa= "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
         for letras in mensaje:
                 num=Abecedarioa.index(letras)
